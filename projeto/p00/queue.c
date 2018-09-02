7a0a1a3d008fb81dfa164dd7bf0042e41fbf5d5e
Enrico Manfron <enricomanfron4@gmail.com>
2018-09-02 14:48:35 -0300

    p00 terminado, foram arrumados alguns errinhos de vetor que pareciam estar certos e dei uma diminuida nas funcoes

--- projeto/p00/queue.c
+++ projeto/p00/queue.c
@@ -5,102 +5,71 @@
 
 
 void queue_append (queue_t **queue, queue_t *elem) {
-	if (queue[0] == NULL){
-     printf ("\nFila criada!\n");
-     queue[0]=elem;
-     elem->prev=elem;
-     elem->next = elem;
+	if(queue == NULL)
+		printf("fila nao criada\n");
+ 	else if (elem == NULL)
+	   	printf ("Elemento nao existe!\n");
+	else if (elem->prev != NULL || elem->next!=NULL)
+	   	printf ("\nElemento ligado a outra fila!\n");
+	else if (*queue == NULL){
+       	printf ("\nFila criada!\n");
+    	*queue=elem;
+    	elem->prev=elem;
+    	elem->next = elem;
    }
-  else if (elem == NULL)
-	   printf ("\nElemento nao existe!\n");
-  else if (elem->prev != NULL || elem->next!=NULL)
-	   printf ("\nElemento ligado a outra fila!\n");
-  else{
-     queue_t *ultimo = (queue[0])->prev;
-     ultimo->next = elem;
-     elem->prev= ultimo;
-     elem->next=(queue[0]);
-     (queue[0])->prev=elem;
+	else {
+     	elem->next=*queue;
+		elem->prev= elem->next->prev;
+		elem->prev->next = elem;
+		elem->next->prev = elem;
   }
 }
 
 queue_t *queue_remove (queue_t **queue, queue_t *elem) {
-   if (queue == NULL){
-  	 printf ("Fila nao existe!");
-  	  return NULL;
-   }
-   else if (queue[0]==NULL){
-     printf ("Fila vazia");
-     return NULL;
-   }
-   else if (elem == 0){
-	    printf ("Elemento nao existe!");
-      return NULL;
-   }
-   else {
-     int tam = queue_size (queue[0]);
-		 if (tam==1 && queue[0]==elem){
-				 queue_t *at = queue[0];
-				 at->next=NULL;
-				 at->prev=NULL;
-				 queue[0]=NULL;
-				 return at;
-		 }
-		 if (tam==1 && queue[0]!=elem){
-			 		printf ("Elemento nao estava na fila");
-					return NULL;
-		 }
-		 int i;
-     for (i=0; i<tam; i++){
-			 		if(queue[i] == elem && i==0){
-							queue_t *at = queue[i];
-							(queue[i])->prev->next=queue[i]->next;
-							(queue[i])->next->prev=queue[i]->prev;
-							queue[i]=(queue[i])->next;
-							at->next=NULL;
-							at->prev=NULL;
-							return at;
-					}
-					else if(queue[i] == elem && i!=0){
-						queue_t *at = queue[i];
-						(queue[i])->prev->next=queue[i]->next;
-						(queue[i])->next->prev=queue[i]->prev;
-						at->next=NULL;
-						at->prev=NULL;
-						return at;
-    	    }
-
+    if (queue == NULL)
+  	 	printf ("Fila nao existe!");
+    else if (*queue==NULL)
+    	printf ("Fila vazia");
+    else {
+    	queue_t *rem ;
+    	for (rem = *queue;rem!=elem && rem->next!=*queue ;rem=rem->next);
+    	if (rem == elem){
+    		if(rem==*queue){
+    			if (rem->next==rem)
+    				*queue=NULL;
+    			else
+    				*queue=rem->next;
+    		}
+    		rem->next->prev=rem->prev;
+    		rem->prev->next=rem->next;
+    		rem->next=NULL;
+    		rem->prev=NULL;
+    		return rem;
     	}
-    	printf ("\nElemento não pertencia a fila!\n");
-	    return NULL;
-
+    	printf("elemento nao existe!\n");
     }
+    return NULL;
 }
 
 int queue_size (queue_t *queue) {
-  if (queue==NULL){
-	   printf ("\nFila vazia!\n");
-     return 0;
-  }
-  int i;
-  queue_t *elem = queue->next;
-  for (i=1; elem!=queue ; i++)
-    elem = elem->next ;
-  return i;
+	if (queue!=NULL){
+		int i;
+		queue_t *elem = queue->next;
+		for (i=1; elem!=queue ; i++)
+			elem = elem->next ;
+		return i;
+	}
+	return 0;
 }
 
 void queue_print (char *name, queue_t *queue, void print_elem (void*)){
-  if (queue==NULL)
-		printf ("[]");
-  else{
-		queue_t *elem=queue;
-		print_elem (elem);
+  	queue_t *elem = queue;
+  	while(queue != NULL && elem->next!=queue){
+  		print_elem (elem);
 		printf (" ");
-		for (elem=elem->next; elem!=queue; elem=elem->next){
-			print_elem (elem);
-			printf (" ");
-		}
+		elem=elem->next;
 	}
+  	print_elem(queue);
 	printf ("\n");
 
 }
